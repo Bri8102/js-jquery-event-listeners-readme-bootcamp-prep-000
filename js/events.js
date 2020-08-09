d6e5da9ac734e4f89106d5ce6ef527493d25c348
Briana Oniru <damioniru16@gmail.com>
2020-08-09 21:44:48 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,10 +3,12 @@
 $(document).ready(function(){
 
 // call functions here
+
+});
+
 function getIt() {
-  $('p').on('click',function() {
+  $('p').on('click', function(){
     alert('Hey!');
   });
 }
-});
-
+  
