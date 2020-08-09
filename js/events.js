4552738bb58eaf80d2ddf21b4c7d7fa904c57b33
Briana Oniru <damioniru16@gmail.com>
2020-08-09 22:21:40 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -18,6 +18,16 @@ function frameIt() {
   });
 }
 
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if (key.which == 71) {
+      alert('g was pressed');
+      return; }
+    if (key.which == 70) {
+      return;}
+    });
+  }
+
 function submitIt() {
   $('form').on('submit', function() {
     alert('Your form is going to be submitted now.');
